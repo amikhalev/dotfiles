d5e339499db66aaa72dca31fe71710afb6da7fb3
Alex Mikhalev <alexmikhalevalex@gmail.com>
2016-10-17 17:14:57 -0600

    Improved install.sh script

--- install.sh
+++ install.sh
@@ -17,9 +17,37 @@ install_rcm_brew() {
 }
 
 install_rcm_linux() {
-    has_command pacaur || { echo "pacaur is not installed. Please install pacaur or install rcm manually."; exit 1; }
-    echo Installing rcm from the AUR. Please accept any prompts
-    pacaur -S rcm
+    if has_command pacaur; then
+        echo Installing rcm from the AUR. Please accept any prompts
+        pacaur -S rcm
+    elif has_command add-apt-repository; then
+        echo Installing rcm from ubuntu ppa
+        sudo add-apt-repository ppa:martin-frost/thoughtbot-rcm
+        sudo apt-get update
+        sudo apt-get install rcm
+    else
+        echo "No known way to install rcm. Please install it manually"
+        exit 1
+    fi
+}
+
+install_git_brew() {
+    echo Installing git from homebrew
+    brew install git
+}
+
+install_git_linux() {
+    if has_command pacman; then
+        echo Installing git from pacman
+        sudo pacman -S git
+    elif has_command apt-get; then
+        echo Installing git from apt-get
+        sudo apt-get update
+        sudo apt-get install git
+    else
+        echo No known way to install git. Please install it manually
+        exit 1
+    fi
 }
 
 if ! has_command rcup; then
@@ -41,7 +69,21 @@ else
 fi
 
 if [ ! -e "$DOTFILES_DIR" ]; then
-    has_command git || { echo "git is not installed and must be in order to fetch dotfiles"; exit 1; }
+    if ! has_command git; then 
+        echo "git is not installed!"    
+        case `uname` in
+            Darwin)
+                install_git_brew
+                ;;
+            Linux)
+                install_git_linux
+                ;;
+            *)
+                echo "Unknown system `uname`. Install git manually"
+                exit 1
+                ;;
+        esac
+    fi
     echo Cloning dotfiles repo
     git clone --recursive $REPO_URL $DOTFILES_DIR
 else
