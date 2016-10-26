100d2779792a531728b290ddeb4575dbfd709f81
Alex Mikhalev <alexmikhalevalex@gmail.com>
2016-10-26 17:25:00 -0600

    Added bashcompinit for using bash completion scripts

--- config/zshrc.d/60-completion.sh
+++ config/zshrc.d/60-completion.sh
@@ -5,5 +5,8 @@
 # Move .zcompdump and .zcompcache to the $XDG_CACHE_HOME folder (cleans up the home directory)
 zstyle ':completion:*:' cache-path $ZSH_CACHE_DIR/zcompcache 
 
+autoload -U +X bashcompinit;
+
 compinit -d $ZSH_CACHE_DIR/zcompdump
+bashcompinit
 
