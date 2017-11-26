e0df729652f92bf41d4370820869b3fd21b37b60
Alex Mikhalev <alexmikhalevalex@gmail.com>
2017-11-26 03:22:59 -0700

    Improved gpg agent cross platform compat

--- zshrc.d/99-gpg-agent.sh
+++ zshrc.d/99-gpg-agent.sh
@@ -1,11 +1,15 @@
-GNUPGHOME=${GNUPGHOME:-"$HOME/.gnupg"}
+GNUPGHOME=$(gpgconf --list-dir homedir)
 
 GPG_TTY=$(tty)
+
+if ! pgrep gpg-agent 1>/dev/null; then
+    gpgconf --launch gpg-agent &
+fi
 
 # Set SSH to use gpg-agent if it is configured to do so
 GNUPGCONFIG="$GNUPGHOME/gpg-agent.conf"
 if [ -r "$GNUPGCONFIG" ] && grep -q enable-ssh-support "$GNUPGCONFIG"; then
   unset SSH_AGENT_PID
-  export SSH_AUTH_SOCK="$GNUPGHOME/S.gpg-agent.ssh"
+  export SSH_AUTH_SOCK=$(gpgconf --list-dir agent-ssh-socket)
 fi
 
