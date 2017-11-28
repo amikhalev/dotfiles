f090bb21bb62ecbb012493f193a5f7b303e3904a
Alex Mikhalev <alexmikhalevalex@gmail.com>
2017-11-28 02:47:25 -0700

    Updated better aliases

--- zshrc.d/50_aliases.zsh
+++ zshrc.d/50_aliases.zsh
@@ -3,12 +3,17 @@ has_command nvim && alias vim=nvim
 
 alias gping="ping 8.8.8.8"
 
-alias cdotfiles="cd $HOME/.dotfiles"
-alias updotfiles="cd $HOME/.dotfiles && git pull --recurse-submodules || cd -"
+alias cdot="cd $DOTFILES"
+updot() {
+    cd $DOTFILES
+    git pull --recurse-submodules
+    cd -
+}
 
 alias t="tail -f"
 
 alias pgen="dd if=/dev/urandom bs=1 count=32 2>/dev/null | base64 | rev | cut -b 2- | rev"
 
-alias edzshrc="$EDITOR ~/.zshrc"
-alias edplug="$EDITOR ~/.zshrc.d/10_zplug.zsh"
+alias vimrc="$EDITOR ~/.vimrc"
+alias vizrc="$EDITOR ~/.zshrc"
+alias vizpl="$EDITOR ~/.zshrc.d/plugins.zsh"
